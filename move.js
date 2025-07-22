c7689649bfa1902bba565e63b0865b507bb060e9
tijimaru <chijiiwasouma0919@gmail.com>
2025-07-22 13:24:54 +0900

    Add files via upload

---
+++ move.js
@@ -0,0 +1,17 @@
+// ページトップに戻るボタンの表示制御と動作
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+});
+
+scrollToTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
